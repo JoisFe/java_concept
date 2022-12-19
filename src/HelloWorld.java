650bb86b4c952b9049e2423d5a62a5064048efd3
조재철 <rhsnqk@gmail.com>
2022-12-19 17:01:38 +0900

    feat: jvm document 추가

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Thread.currentThread();
+    }
+
+}
